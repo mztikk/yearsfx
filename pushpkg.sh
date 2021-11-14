fc2bdd45cb1b8095a434973f38ee1fd6326fafb8
mztikk <mztikk@outlook.de>
2021-11-14 01:10:39 +0100

    pushpkg makepkg -g

--- pushpkg.sh
+++ pushpkg.sh
@@ -3,6 +3,7 @@ msg=$(cargo run -- --version)
 cp PKGBUILD ../packages/yearsfx/
 cd ../packages/yearsfx
 makepkg --printsrcinfo > .SRCINFO
+makepkg -g >> PKGBUILD
 git add PKGBUILD .SRCINFO
 git commit -m "$msg"
 git push
